e1608277f6a24dd295be88f1b194401e9665ea27
jar100 <jar10038@gmail.com>
2020-01-31 11:05:11 +0900

    url 환경변수로 뺌

--- src/main/java/com/lq/slackbot/controller/Scheduler.java
+++ src/main/java/com/lq/slackbot/controller/Scheduler.java
@@ -12,11 +12,12 @@ import java.util.Objects;
 @Component
 @Slf4j
 public class Scheduler {
+	private static final String BASE_URL = System.getenv("SLACKBOT_BASE_URL");
 
 	@Scheduled(cron="0 */15 * * * *")
 	public void check() {
 		log.info("실행시간 : {}", LocalDateTime.now());
-		final WebClient webClient = WebClient.create("https://aqueous-plains-22889.herokuapp.com");
+		final WebClient webClient = WebClient.create(BASE_URL);
 		final Mono<String> stringMono = Objects.requireNonNull(webClient.get().uri("/init").exchange().block()).bodyToMono(String.class);
 		log.info("nonSleep : {}",stringMono.block());
 	}
