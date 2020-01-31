aae81e7960f5fdc03d1d2eb98b21bca342c6c5ce
jar100 <jar10038@gmail.com>
2020-02-01 00:43:39 +0900

    점심추천봇-2

--- src/main/java/com/lq/slackbot/service/MessageEventService.java
+++ src/main/java/com/lq/slackbot/service/MessageEventService.java
@@ -3,10 +3,12 @@ package com.lq.slackbot.service;
 import com.lq.slackbot.domain.MessageEventType;
 import com.lq.slackbot.domain.Restaurant;
 import com.lq.slackbot.domain.SlackRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 @Service
+@Slf4j
 public class MessageEventService {
 	private MessageService messageService;
 
@@ -19,13 +21,14 @@ public class MessageEventService {
 
 		final String text = request.getEvent().getText();
 		if (StringUtils.isEmpty(text)) {
+			log.error("택스트를 찾을 수 없음");
 			return;
 		}
 		if (text.contains(MessageEventType.LUNCH.name())) {
 			final int random = (int) Math.round(Math.random() * (Restaurant.values().length - 1));
 			message = Restaurant.of(random).getName();
 		}
-
+		log.info(message);
 		messageService.sendMessageV3(request.getChannel(),message);
 	}
 }
