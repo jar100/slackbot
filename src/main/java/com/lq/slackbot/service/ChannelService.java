2e367dfbf172b055fdef207983dd3e6ee5db8013
jar100 <jar10038@gmail.com>
2020-02-26 01:10:12 +0900

    config setting2

--- src/main/java/com/lq/slackbot/service/ChannelService.java
+++ src/main/java/com/lq/slackbot/service/ChannelService.java
@@ -3,11 +3,13 @@ package com.lq.slackbot.service;
 import com.lq.slackbot.domain.Channel;
 import com.lq.slackbot.domain.ChannelRepository;
 import com.lq.slackbot.domain.ChannelResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ChannelService {
 	private ChannelRepository channelRepository;
@@ -21,6 +23,7 @@ public class ChannelService {
 	}
 
 	public void deleteAll() {
+		log.info("channels : {}", channelRepository.findAll());
 		channelRepository.deleteAll();
 	}
 }
