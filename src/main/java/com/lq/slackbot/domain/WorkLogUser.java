7e243d8613f61814736da4d47d9e1a12123a1633
jar100 <jar10038@gmail.com>
2020-03-16 22:16:30 +0900

    log 추가

--- src/main/java/com/lq/slackbot/domain/WorkLogUser.java
+++ src/main/java/com/lq/slackbot/domain/WorkLogUser.java
@@ -3,6 +3,8 @@ package com.lq.slackbot.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,9 +12,11 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class WorkLogUser {
 	private String id;
 	private String email;
@@ -24,12 +28,14 @@ public class WorkLogUser {
 	public WorkLogRequest toWorkLogRequest(String type) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 		ZoneOffset seoulZoneOffset = ZoneOffset.of("+09:00");
+		final String time = ZonedDateTime.now(seoulZoneOffset).toString();
+		log.info("존데이트타임 time : {}", time);
 		return WorkLogRequest.builder()
 				.type(type)
 				.user_id(this.id)
 				.auth_user_id(this.userUid+123)
 				.target_date(LocalDate.now().format(formatter))
-				.time(ZonedDateTime.now(seoulZoneOffset).toString())
+				.time(time)
 				.build();
 	}
 
