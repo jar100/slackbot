60be3a65c2f611619ea7a5ba9fec28def3a257c3
jar100 <jar10038@gmail.com>
2020-06-23 16:43:20 +0900

    정석님생일수정

--- src/main/java/com/lq/slackbot/domain/BirthdayImg.java
+++ src/main/java/com/lq/slackbot/domain/BirthdayImg.java
@@ -4,6 +4,7 @@ public enum BirthdayImg {
 	ONE("https://user-images.githubusercontent.com/39078045/78988597-56baf980-7b6c-11ea-8f91-196629cbecc4.jpg"),
 	TWO("https://user-images.githubusercontent.com/39078045/81413509-8582ba80-9180-11ea-9dee-b6586053e836.png"),
 	PHOTO_1("https://user-images.githubusercontent.com/39078045/84565737-24b15800-ada6-11ea-8ebf-4f6588b4f2d6.png"),
+	PHOTO_2("https://user-images.githubusercontent.com/39078045/85374956-87f27580-b570-11ea-84d5-799b2b6d5785.png"),
 
 
 
