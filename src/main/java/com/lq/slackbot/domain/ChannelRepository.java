6d73469ec4eed760b0f9456fc5a506332523aca1
jar100 <jar10038@gmail.com>
2020-10-05 10:34:31 +0900

    어싱크 수정2

--- src/main/java/com/lq/slackbot/domain/ChannelRepository.java
+++ src/main/java/com/lq/slackbot/domain/ChannelRepository.java
@@ -2,10 +2,7 @@ package com.lq.slackbot.domain;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface ChannelRepository extends JpaRepository<Channel,String> {
+public interface ChannelRepository extends JpaRepository<Channel, String> {
 
-	@Query("select NEW com.lq.slackbot.domain.Actions(i.erpPartnerCode,count(erp_shop_code),count(case when i.activated = true then 1 )) from ErpPartner as i where i.erpPartnerCode = ?1 group by i.erpPartnerCode\"")
-	Channel findBy();
 }
